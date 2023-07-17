03c1764ebb861f767e47751242e51d8897872652
Miraak <45670960+amirjoker710@users.noreply.github.com>
2023-07-17 13:05:01 +0330

    Fisrt Ui test
    طراحی ui ساده

--- Version 1-1-1/Form1.cs
+++ Version 1-1-1/Form1.cs
@@ -6,5 +6,25 @@ namespace Version_1_1_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
